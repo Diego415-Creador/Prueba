73be6c4b1ff2af3e2d97f74d1eed6f4184d29c9c
Diego <gamezdiego07@gmail.com>
2020-07-10 14:18:24 -0500

    Primer codigo

---
+++ tiposdedatos.py
@@ -0,0 +1,17 @@
+#Programa para calcular las raices de una ecuacion de segundo grado
+from math import sqrt
+
+
+a = 1
+b = 0
+c = 1
+
+x_1 = 0
+x_2 = 0
+
+x1 = (-b + cmath.sqtr(b**2 - 4*a*C)/2
+x2 = (-b - cmath.sqtr(b**2 - 4*a*C)/2
+
+print(x_1, 'n')
+  print(x_2)
+ 
